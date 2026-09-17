bb8c49440cc7d779735f7b3b619843c13c27e66f
Mônica Cotrim Manfrinato <monica.manfrido@aluno.senai.br>
2026-09-17 16:08:11 -0300

    codigo TODO

--- src/services/api.js
+++ src/services/api.js
@@ -2,19 +2,66 @@
 const BASE_URL = "http://10.0.2.2:3000";
 
 export async function buscarLivros() {
-  // TODO: implementar
+  try{
+    const response = await fetch(`${BASE_URL}/livros`)
+    if (!response.ok){
+      throw new Error(`Erro ${response.status} : falha ao buscar livros`)
+    }
+    return response.json()
+  }
+  catch(e){
+    console.error('buscarLivros:', e.message)
+  }
 }
 
 export async function buscarLivroPorId(id) {
-  // TODO: implementar
+  try{
+    const response = await fetch(`${BASE_URL}/livros/${id}`)
+    if (!response.ok){
+      throw new Error(`Erro ${response.status} : status não encontrado`)
+    }
+    return response.json()
+
+  }
+  catch(e){
+    console.error('buscarLivroPorId:', e.message)
+  }
 }
 
-export async function adicionarFavorito(livroId, observacao) {
-  // TODO: implementar
+export async function adicionarFavorito(livroId, observacao = "") {
+  try{
+    const response = await fetch(`${BASE_URL}/favoritos,`, {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({livroId, observacao})
+    })
+
+    if (!response.ok){
+      const corpo = await response.json().catch(()=>({}))
+      const erro = new Error(corpo.erro ?? `Erro ${response.status} : falha ao adicionar favorito` )
+      erro.status = response.status
+      throw erro
+    }
+    return response.json()
+
+  }
+  catch(e){
+    console.error('adicionarFavorito:', e.message)
+  }
 }
 
 export async function listarFavoritos() {
-  // TODO: implementar
+  try{
+    const response = await fetch(`${BASE_URL}/favoritos`)
+    if (!response.ok){
+      throw new Error(`Erro ${response.status} : falha ao listar favoritos`)
+    }
+    return response.json()
+
+  }
+  catch(e){
+    console.error('listarFavoritos:', e.message)
+  }
 }
 
 export async function editarFavorito(id, observacao) {
